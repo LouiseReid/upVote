4a78bdaec557a31e1b595497eb958dff0b565b98
Louise Reid <l_reid@hotmail.co.uk>
2019-01-16 20:23:46 +0000

    thumbs linked in

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, Platform } from 'react-native';
 import CustomText from '../custom_components/CustomText';
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const Comment = ({comment}) => (
 
@@ -8,7 +9,19 @@ const Comment = ({comment}) => (
       <CustomText style={styles.userName}>{comment.item.user_name}</CustomText>
       <CustomText style={styles.date}>{comment.item.date}</CustomText>
       <CustomText style={styles.comment}>{comment.item.comment}</CustomText>
-      <CustomText style={styles.likes}>{comment.item.likes}</CustomText>
+      <View style={styles.likesContainer}>
+        <Icon
+          name={Platform.OS === 'ios' ? 'ios-thumbs-up' : 'md-thumbs-up'}
+          size={15}
+          style={styles.thumbIcon}
+        />
+        <Icon
+          name={Platform.OS === 'ios' ? 'ios-thumbs-down' : 'md-thumbs-down'}
+          size={15}
+          style={styles.thumbIcon}
+        />
+        <CustomText style={styles.likes}>{comment.item.likes}</CustomText>
+      </View>
     </View>
 )
 
@@ -32,10 +45,19 @@ const styles = StyleSheet.create({
     paddingTop: 5,
     paddingBottom: 5
   },
+  likesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
   likes: {
     alignSelf: 'flex-end',
     color:  '#2b4c6b',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginLeft: 15
+  },
+  thumbIcon: {
+    marginLeft: 15,
+    color:  '#2b4c6b'
   }
 })
 
